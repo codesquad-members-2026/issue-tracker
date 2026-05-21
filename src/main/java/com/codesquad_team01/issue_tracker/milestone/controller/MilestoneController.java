ff5d457180d8a1ae76387fe91eedcd2cceadf189
LeeJaeWan <jjjkuul@naver.com>
2026-05-22 07:07:56 +0900

    Feat: MilestoneController 마일스톤 삭제 API 및 응답 규격 구현
    - DELETE /api/milestones/{milestoneId} 엔드포인트를 추가하여 RESTful 설계 원칙 준수
    - @Validated와 @Min 애너테이션을 활용해 PathVariable로 들어오는 ID 값의 유효성 검증(1 이상의 양수) 로직 추가

--- src/main/java/com/codesquad_team01/issue_tracker/milestone/controller/MilestoneController.java
+++ src/main/java/com/codesquad_team01/issue_tracker/milestone/controller/MilestoneController.java
@@ -2,14 +2,15 @@ package com.codesquad_team01.issue_tracker.milestone.controller;
 
 import com.codesquad_team01.issue_tracker.global.dto.ApiResponse;
 import com.codesquad_team01.issue_tracker.milestone.domain.MilestoneState;
+import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneDeleteResponse;
 import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneListResponse;
 import com.codesquad_team01.issue_tracker.milestone.service.MilestoneService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import jakarta.validation.constraints.Min;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@Validated
 @RequestMapping("/api/milestones")
 public class MilestoneController {
     private final MilestoneService milestoneService;
@@ -25,5 +26,13 @@ public class MilestoneController {
         String message = state == MilestoneState.OPEN ?
                 "열린 모든 마일스톤 조회 성공" : "닫힌 모든 마일스톤 조회 성공";
         return ApiResponse.success(message, milestoneService.findMilestones(state));
+    }
+
+    @DeleteMapping("/{milestoneId}")
+    public ApiResponse<MilestoneDeleteResponse> deleteMilestone (
+            @PathVariable @Min(value = 1, message = "ID는 1 이상의 양수여야 합니다.") Long milestoneId
+    ){
+
+        return ApiResponse.success("마일스톤 삭제 성공", milestoneService.deleteMilestone(milestoneId));
     }
 }
