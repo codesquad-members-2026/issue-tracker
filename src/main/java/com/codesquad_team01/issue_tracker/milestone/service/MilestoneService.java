6f920ce57d087781add1ebd9c164bfb1c7f7063f
LeeJaeWan <jjjkuul@naver.com>
2026-05-22 07:07:32 +0900

    Feat: MilestoneService 조회 및 삭제 비즈니스 로직 구현
    - 클래스 레벨에 @Transactional(readOnly = true)를 적용하여 불필요한 스냅샷 생성을 막고 전반적인 조회 성능 최적화
    - 데이터베이스 상태를 변경하는 deleteMilestone 메서드에 쓰기 전용 @Transactional을 적용하여 작업의 원자성(Atomicity) 보장
    - Repository의 boolean 반환값을 검증하여, 존재하지 않거나 이미 삭제된 마일스톤에 대한 방어 및 예외 처리 로직 추가

--- src/main/java/com/codesquad_team01/issue_tracker/milestone/service/MilestoneService.java
+++ src/main/java/com/codesquad_team01/issue_tracker/milestone/service/MilestoneService.java
@@ -1,10 +1,13 @@
 package com.codesquad_team01.issue_tracker.milestone.service;
 import com.codesquad_team01.issue_tracker.milestone.domain.MilestoneState;
+import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneDeleteResponse;
 import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneListResponse;
 import com.codesquad_team01.issue_tracker.milestone.repository.MilestoneRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 public class MilestoneService {
     private final MilestoneRepository milestoneRepository;
 
@@ -14,5 +17,16 @@ public class MilestoneService {
 
     public MilestoneListResponse findMilestones(MilestoneState state){
         return new MilestoneListResponse(milestoneRepository.findAllByState(state));
+    }
+
+    @Transactional
+    public MilestoneDeleteResponse deleteMilestone(Long milestoneId){
+        boolean idDeleted = milestoneRepository.deleteMilestoneById(milestoneId);
+
+        if(!idDeleted){
+            throw new IllegalStateException("마일스톤이 존재하지 않거나 이미 삭제됐습니다."); // TODO: 추후 404로 예외처리
+        }
+
+        return MilestoneDeleteResponse.from(milestoneId);
     }
 }
