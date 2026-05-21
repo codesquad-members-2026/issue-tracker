60efbbb38a35ecef5a6d3ce114dac8aca7bf7f02
LeeJaeWan <jjjkuul@naver.com>
2026-05-22 08:33:46 +0900

    Feat: MilestoneService 트랜잭션 기반 상태 변경 로직 구현
    - 데이터베이스 상태를 변경하는 메서드에 쓰기 전용 @Transactional을 적용하여 작업의 원자성(Atomicity) 보장
    - 상태 변경 로직에 'UPDATE 쿼리 실행 후 최신 스냅샷 SELECT' 파이프라인을 구축하여 I/O 효율과 데이터 정합성 동시 확보
    - Repository의 UPDATE 반환값(boolean)을 검증하여, 식별자가 존재하지 않거나 이미 논리적 삭제된 데이터에 대한 선제적 예외 처리(추후 404 전환) 적용

--- src/main/java/com/codesquad_team01/issue_tracker/milestone/service/MilestoneService.java
+++ src/main/java/com/codesquad_team01/issue_tracker/milestone/service/MilestoneService.java
@@ -1,6 +1,7 @@
 package com.codesquad_team01.issue_tracker.milestone.service;
 import com.codesquad_team01.issue_tracker.milestone.domain.MilestoneState;
 import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneDeleteResponse;
+import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneListItemResponse;
 import com.codesquad_team01.issue_tracker.milestone.dto.response.MilestoneListResponse;
 import com.codesquad_team01.issue_tracker.milestone.repository.MilestoneRepository;
 import org.springframework.stereotype.Service;
@@ -20,11 +21,23 @@ public class MilestoneService {
     }
 
     @Transactional
+    public MilestoneListItemResponse updateMilestoneState(MilestoneState state, Long milestoneId){
+        boolean isUpdated = milestoneRepository.updateMilestoneState(state, milestoneId);
+
+        // TODO: 추후 404로 예외처리
+        if(!isUpdated){
+            throw new IllegalStateException("마일스톤이 이미 삭제됐거나 존재하지 않습니다.");
+        }
+
+        return milestoneRepository.findByIdWithCounts(milestoneId).orElseThrow(IllegalStateException::new);
+    }
+
+    @Transactional
     public MilestoneDeleteResponse deleteMilestone(Long milestoneId){
         boolean idDeleted = milestoneRepository.deleteMilestoneById(milestoneId);
 
         if(!idDeleted){
-            throw new IllegalStateException("마일스톤이 존재하지 않거나 이미 삭제됐습니다."); // TODO: 추후 404로 예외처리
+            throw new IllegalStateException("마일스톤이 이미 삭제됐거나 존재하지 않습니다."); // TODO: 추후 404로 예외처리
         }
 
         return MilestoneDeleteResponse.from(milestoneId);
