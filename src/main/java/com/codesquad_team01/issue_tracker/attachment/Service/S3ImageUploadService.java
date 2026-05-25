4999d3eaf7fc5b6e2eee58a370d9b7d45389ff16
Wonjae-Song0906 <151840746+Wonjae-Song0906@users.noreply.github.com>
2026-05-25 22:12:59 +0900

    feat: s3 버킷 사용, 이미지 업로드 로직 구현

---
+++ src/main/java/com/codesquad_team01/issue_tracker/attachment/Service/S3ImageUploadService.java
@@ -0,0 +1,53 @@
+package com.codesquad_team01.issue_tracker.attachment.Service;
+
+import io.awspring.cloud.s3.S3Resource;
+import io.awspring.cloud.s3.S3Template;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class S3ImageUploadService {
+
+    private final S3Template s3Template;
+
+    @Value("${spring.cloud.aws.s3.bucket}")
+    private String bucket;
+
+    public String uploadImage(MultipartFile file) {
+
+        if (file.isEmpty()) {
+            throw new IllegalArgumentException("업로드할 파일이 비어있습니다.");
+        }
+
+        String originalFilename = file.getOriginalFilename();
+        String storeFilename = createStoreFilename(originalFilename);
+
+        try (InputStream inputStream = file.getInputStream()) {
+
+            S3Resource s3Resource = s3Template.upload(bucket, storeFilename, inputStream);
+
+            return s3Resource.getURL().toString();
+
+        } catch (IOException e) {
+            throw new RuntimeException("S3 파일 업로드 중 오류가 발생했습니다.", e);
+        }
+    }
+
+    private String createStoreFilename(String originalFilename) {
+        String ext = extractExt(originalFilename);
+        String uuid = UUID.randomUUID().toString();
+        return uuid + "." + ext;
+    }
+
+    private String extractExt(String originalFilename) {
+        int pos = originalFilename.lastIndexOf(".");
+        return originalFilename.substring(pos + 1);
+    }
+}
---
+++ src/main/java/com/codesquad_team01/issue_tracker/attachment/dto/response/ImageUploadResponse.java
@@ -0,0 +1,6 @@
+package com.codesquad_team01.issue_tracker.attachment.dto.response;
+
+public record ImageUploadResponse (
+    String imageUrl
+)
+{}
