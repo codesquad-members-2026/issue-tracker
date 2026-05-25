fd70e5ebed6fecc9ee9ef3e6599290d1464a1c71
Wonjae-Song0906 <151840746+Wonjae-Song0906@users.noreply.github.com>
2026-05-25 22:49:58 +0900

    feat: 첨부하는 파일의 컨텐트 타입을 정확하게 알아내기 위한 로직 추가

--- src/main/java/com/codesquad_team01/issue_tracker/attachment/Service/S3ImageUploadService.java
+++ src/main/java/com/codesquad_team01/issue_tracker/attachment/Service/S3ImageUploadService.java
@@ -1,5 +1,6 @@
 package com.codesquad_team01.issue_tracker.attachment.Service;
 
+import io.awspring.cloud.s3.ObjectMetadata;
 import io.awspring.cloud.s3.S3Resource;
 import io.awspring.cloud.s3.S3Template;
 import lombok.RequiredArgsConstructor;
@@ -32,7 +33,8 @@ public class S3ImageUploadService {
 
         try (InputStream inputStream = file.getInputStream()) {
 
-            S3Resource s3Resource = s3Template.upload(bucket, storeFilename, inputStream);
+            S3Resource s3Resource = s3Template.upload(bucket, storeFilename, inputStream,
+                    ObjectMetadata.builder().contentType(file.getContentType()).build());
 
             return s3Resource.getURL().toString();
 
