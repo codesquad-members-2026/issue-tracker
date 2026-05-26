e2b4a49c7ae15d3fb0f3840833876adef91feb88
LeeJaeWan <jjjkuul@naver.com>
2026-05-26 17:46:50 +0900

    Feat: GithubOauthClient getAccessToken() 구현
    - 프론트엔드에게 받은 code를 통해 Github 서버에 Access Token을 발급받음
    - 환경 변수로 redirect-uri, token-url, user-url 값을 지정

--- src/main/java/com/codesquad_team01/issue_tracker/auth/GithubOauthClient.java
+++ src/main/java/com/codesquad_team01/issue_tracker/auth/GithubOauthClient.java
@@ -1,7 +1,12 @@
 package com.codesquad_team01.issue_tracker.auth;
 
+import com.codesquad_team01.issue_tracker.auth.dto.request.GithubTokenRequest;
+import com.codesquad_team01.issue_tracker.auth.dto.response.GithubTokenResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.web.client.RestClient;
+
+import java.util.Optional;
 
 public class GithubOauthClient {
 
@@ -21,10 +26,21 @@ public class GithubOauthClient {
         this.restClient = RestClient.create();
     }
 
-//    public String getAccessToken(String code){
-//
-//        return "";
-//    }
+    public String getAccessToken(String code){
+        return Optional.ofNullable(
+                restClient.post()
+                .uri("https://github.com/login/oauth/access_token")
+                .header("Accept", "application/json")
+                .body(new GithubTokenRequest(clientId, clientSecret, code, redirectUri))
+                .retrieve()
+                .onStatus(HttpStatusCode::isError, (request, res) -> {
+                    throw new RuntimeException("GitHub Access Token 발급 실패");
+                })
+                .body(GithubTokenResponse.class)
+        )
+                .map(GithubTokenResponse::accessToken)
+                .orElseThrow(() -> new RuntimeException("GitHub Access Token 발급 실패"));
+    }
 //
 //    public GithubProfile getUserProfile(String accessToken){
 //
