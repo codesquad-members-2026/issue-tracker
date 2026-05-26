2a6372abd610edf3b357d69eb2b32d26a462fe7d
LeeJaeWan <jjjkuul@naver.com>
2026-05-26 17:50:17 +0900

    Feat: oauth:github: 의 환경 변수 추가

--- src/main/java/com/codesquad_team01/issue_tracker/auth/GithubOauthClient.java
+++ src/main/java/com/codesquad_team01/issue_tracker/auth/GithubOauthClient.java
@@ -3,7 +3,9 @@ package com.codesquad_team01.issue_tracker.auth;
 import com.codesquad_team01.issue_tracker.auth.dto.request.GithubTokenRequest;
 import com.codesquad_team01.issue_tracker.auth.dto.response.GithubTokenResponse;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
 import org.springframework.web.client.RestClient;
 
 import java.util.Optional;
@@ -13,24 +15,30 @@ public class GithubOauthClient {
     private final String clientId;
     private final String clientSecret;
     private final String redirectUri;
+    private final String tokenUrl;
+    private final String userUrl;
     private final RestClient restClient;
 
     public GithubOauthClient(
             @Value("${oauth.github.client-id}") String clientId,
             @Value("${oauth.github.client-secret}") String clientSecret,
-            @Value("${oauth.github.redirect-uri}") String redirectUri
+            @Value("${oauth.github.redirect-uri}") String redirectUri,
+            @Value("${oauth.github.token-url}") String tokenUrl,
+            @Value("%{oauth.github.user-url}") String userUrl
     ) {
         this.clientId = clientId;
         this.clientSecret = clientSecret;
         this.redirectUri = redirectUri;
+        this.tokenUrl = tokenUrl;
+        this.userUrl = userUrl;
         this.restClient = RestClient.create();
     }
 
     public String getAccessToken(String code){
         return Optional.ofNullable(
                 restClient.post()
-                .uri("https://github.com/login/oauth/access_token")
-                .header("Accept", "application/json")
+                .uri(tokenUrl)
+                .header(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE)
                 .body(new GithubTokenRequest(clientId, clientSecret, code, redirectUri))
                 .retrieve()
                 .onStatus(HttpStatusCode::isError, (request, res) -> {
