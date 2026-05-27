16a42c61732abd227f66e0ac04fb20c09a8ae76b
LeeJaeWan <jjjkuul@naver.com>
2026-05-27 14:00:13 +0900

    Feat: 댓글 등록 및 조회 기능 구현
    - CommentInput, CommentItem 컴포넌트 추가 및 Markdown 지원
    - 백엔드 댓글 서비스 및 DTO 구현

--- src/main/java/com/codesquad_team01/issue_tracker/comment/dto/request/CommentRequest.java
+++ src/main/java/com/codesquad_team01/issue_tracker/comment/dto/request/CommentRequest.java
@@ -2,9 +2,12 @@ package com.codesquad_team01.issue_tracker.comment.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
 
+import java.util.List;
+
 public record CommentRequest(
 
         @NotBlank(message = "댓글 내용은 필수입니다.")
-        String contents
+        String contents,
+        List<Long> attachmentIds
 ) {
 }
