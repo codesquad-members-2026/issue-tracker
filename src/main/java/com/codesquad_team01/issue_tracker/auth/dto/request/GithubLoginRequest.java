e39c71c6fcfcae55bda0472c429b542f6cccc23c
LeeJaeWan <jjjkuul@naver.com>
2026-05-28 00:51:51 +0900

    Refactor: 인증 관련 DTO 네이밍 변경 및 커스텀 에러 코드 추가
    - LoginRequest -> GithubLoginRequest 변경
    - GithubLoginResponse -> LoginResponse 범용 응답 객체로 변경
    - 로컬 로그인 실패 및 중복 가입 에러 코드 추가

--- src/main/java/com/codesquad_team01/issue_tracker/auth/dto/request/GithubLoginRequest.java
+++ src/main/java/com/codesquad_team01/issue_tracker/auth/dto/request/GithubLoginRequest.java
@@ -1,6 +1,6 @@
 package com.codesquad_team01.issue_tracker.auth.dto.request;
 
-public record LoginRequest(
+public record GithubLoginRequest(
         String code
 ) {
 }
--- src/main/java/com/codesquad_team01/issue_tracker/auth/dto/response/LoginResponse.java
+++ src/main/java/com/codesquad_team01/issue_tracker/auth/dto/response/LoginResponse.java
@@ -2,7 +2,7 @@ package com.codesquad_team01.issue_tracker.auth.dto.response;
 
 import com.codesquad_team01.issue_tracker.member.dto.response.MemberLoginResponse;
 
-public record GithubLoginResponse(
+public record LoginResponse(
         JwtTokenResponse token,
         MemberLoginResponse userLoginResponse
 ) {
